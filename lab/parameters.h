20eeb94e7e27d35f970afba41a271135cc80bce8
Blzbrg <randomparanoid@gmail.com>
2016-11-01 17:31:31 -0400

    minimal project boilerplate
    protothreads but no TFT. main.c, config.h, and include.h

---
+++ lab/parameters.h
@@ -0,0 +1,50 @@
+/**
+ * @file   parameters.h
+ * @author Istvan Burbank, Peter Friend, James Cassell
+ * @date   2016-10-23
+ * @brief  Compile-time parameters to control system.
+ *
+ * Parameters set here are ones that the program adapts to. The set of all
+ * parameters should completely configure the reasonably variable parts of the
+ * system.
+ */
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+/*******************************/
+/* Includes                    */
+/*******************************/
+//@{
+
+//@}
+
+/*******************************/
+/* GLOBAL Macro Definitions    */
+/*******************************/
+//@{
+
+
+//@}
+
+/*******************************/
+/* GLOBAL Variable Definitions */
+/*******************************/
+//@{
+
+//@}
+
+/********************************/
+/* GLOBAL Variable Declarations */
+/********************************/
+//@{
+
+//@}
+
+/********************************/
+/* GLOBAL Function Declarations */
+/********************************/
+//@{
+
+//@}
+
+#endif // PARAMETERS_H
