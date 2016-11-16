6816123a9912eb8d2c78f136da049d5236a5ba63
Blzbrg <randomparanoid@gmail.com>
2016-11-16 12:35:35 -0500

    Center and scale joystick values
    Now sign means something and zero is neutral. Tuning will almost
    definitely be required.

--- lab/parameters.h
+++ lab/parameters.h
@@ -22,4 +22,22 @@
  */
 #define RENDERING_TAB_WIDTH (4)
 
+/*
+ * Controls conversion of ADC values into nice direction vectors.
+ */
+
+// size of range of positive part of joystick
+/* Preliminarily set to 512 because this is half of the range of the unsigned 10
+   bit ADC. Thus from 512 to 1024 is suppoed to be a positive setting. */
+#define JOYSTICK_RAW_POS_RANGE (1024/2)
+// where in the ADC range the neutral position of the joystick is
+#define JOYSTICK_RAW_MIDDLE (1024/2)
+// size of range of negative part of joystick.
+/* Preliminarily set to 512 because this is half of the range of the unsigned 10
+   bit ADC. Thus from 0 to 512 is suppoed to be a negative setting. */
+#define JOYSTICK_RAW_NEG_RANGE (1024/2)
+// range of positive or negative part of output value of joystick (full range is
+// twice this)
+#define JOYSTICK_OUTPUT_RANGE (100)
+
 #endif // PARAMETERS_H
