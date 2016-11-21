63b70f1d4f438b60ed4661d8a09442887a7fc1ef
Blzbrg <randomparanoid@gmail.com>
2016-11-21 17:47:47 -0500

    Boilerplate for GUI files

---
+++ lab/gui.h
@@ -0,0 +1,54 @@
+/**
+ * @file   gui.h
+ * @author Istvan Burbank, Peter Friend, James Cassell
+ * @date   2016-11-21
+ * @brief  Relatively high level GUI
+ */
+
+#ifndef GUI_H
+#define GUI_H
+
+/*******************************/
+/* Includes                    */
+/*******************************/
+//@{
+
+#include "include.h"
+#include "parameters.h"
+
+#include "cursor.h"
+#include "joystick.h"
+#include "spatial.h"
+
+//@}
+
+/*******************************/
+/* GLOBAL Macro Definitions    */
+/*******************************/
+//@{
+
+//@}
+
+/*********************************/
+/* GLOBAL Type(def) Declarations */
+/*********************************/
+//@{
+
+//@}
+
+/********************************/
+/* GLOBAL Variable Declarations */
+/********************************/
+//@{
+
+//@}
+
+/********************************/
+/* GLOBAL Function Declarations */
+/********************************/
+//@{
+
+//@}
+
+#endif // GUI_H
+
