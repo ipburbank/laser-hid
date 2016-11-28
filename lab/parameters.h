6526ec3d633766aab72cb6349529ed4eb4f65121
Blzbrg <randomparanoid@gmail.com>
2016-11-28 00:02:00 -0500

    Cursor unit conversion in terms of joystick range
    Rather than hardcoded magic number, is derived from pound-define for
    joystick output range. SO thankfully dredged up C-spec section 6.10.3.4
    as a reference for this being allowed.

--- lab/parameters.h
+++ lab/parameters.h
@@ -48,6 +48,6 @@
 // the way to an extreme to move a pixel per tick, smaller values mean the
 // joystick must be moved less to get movement and can be moved further for
 // faster movement.
-#define CURSOR_JOYSTICK_UNITS_PER_PIXEL_PER_TICK (1024/2)
+#define CURSOR_JOYSTICK_UNITS_PER_PIXEL_PER_TICK (JOYSTICK_OUTPUT_RANGE/2)
 
 #endif // PARAMETERS_H
