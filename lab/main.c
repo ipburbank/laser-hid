141918190bd51a2a4a77a090b2eeca160d3c0ccf
James Cassell <jcc384@cornell.edu>
2016-12-07 06:37:31 -0500

    Allow user to write morse to display over UART
    User is able to draw morse code on the display to the first row
    only currently. This is due to not having too good rows right now.
    User writes it over UART, command is formatted as
    <string> <red color level> <blue color level> <green color level>

--- lab/main.c
+++ lab/main.c
@@ -35,6 +35,9 @@
 /*******************************/
 //@{
 
+// Size of buffer for UART
+#define BUFFERSIZE (12)
+
 //@}
 
 /*******************************/
@@ -49,6 +52,9 @@
 /*******************************/
 //@{
 
+// Recieves string from user and prints it to the display
+static PT_THREAD(protothread_uartThread(struct pt *pt));
+
 //@}
 
 /*******************************/
@@ -56,12 +62,23 @@
 /*******************************/
 //@{
 
+// Variable used to kepp track of the string we read over UART
+static char cmd[BUFFERSIZE];
+// Red level from user
+static int red;
+// Green level from user
+static int green;
+// Blue level from user
+static int blue;
+
 //@}
 
 /*******************************/
 /* GLOBAL Variable Definitions */
 /*******************************/
 //@{
+
+static struct pt pt_uartThread, pt_input, pt_DMA_output;
 
 //@}
 
@@ -77,6 +94,7 @@ int main(void) {
   ///////////////////////
 
   PT_setup();
+  PT_INIT(&pt_uartThread);
 
   //////////////////////
   /* Set Up Projector */
@@ -95,7 +113,7 @@ int main(void) {
   ///////////////////////
 
   while (1) {
-    // e.g. PT_SCHEDULE(protothread_uartThread(&pt_uartThread));
+    PT_SCHEDULE(protothread_uartThread(&pt_uartThread));
   }
 } // main
 
@@ -113,5 +131,35 @@ int main(void) {
 /*******************************/
 //@{
 
+static PT_THREAD(protothread_uartThread(struct pt *pt)) {
+  PT_BEGIN(pt);
+
+  while (1) {
+
+    // Send propmt to DMA for serial communication and spawn thread to print
+    sprintf(PT_send_buffer, "String to display:");
+    PT_SPAWN(pt, &pt_DMA_output, PT_DMA_PutSerialBuffer(&pt_DMA_output));
+
+    // create thread to deal with input
+    PT_SPAWN(pt, &pt_input, PT_GetSerialBuffer(&pt_input));
+
+    // parse the string returned to us
+    sscanf(PT_term_buffer, "%s %d %d %d", cmd, &red, &blue, &green);
+
+    // Erase the old morse code string
+    rendering_drawMorseString(0, 0, cmd, color_blank);
+
+    // Decoding the command that we recieved
+    // Draw the new line of morse code
+    struct color output_color = {
+      .red = red,
+      .blue = blue,
+      .green = green
+    };
+    rendering_drawMorseString(0, 0, cmd, output_color);
+    // never exit while
+  } // END WHILE(1)
+  PT_END(pt);
+}
 //@}
 
